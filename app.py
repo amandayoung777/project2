b42f3085cd9d94675bac49c849c0bad9b1b1ce49
amandayoung777 <amanda.young777@gmail.com>
2022-05-03 20:29:55 +1000

    updated

--- app.py
+++ app.py
@@ -26,12 +26,58 @@ def mood_action():
     print(mood_request)
     conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
-    cur.execute('SELECT name FROM food WHERE mood=%s', [mood_request])
+    cur.execute('SELECT name FROM food WHERE mood=%s;', [mood_request])
     results = cur.fetchall()
     conn.commit()
     conn.close()
     print(results)
-    return render_template('food.html', mood_request = mood_request)
+    # for result in range(len(results)):
+    #     print(results[result], end="")
+    # initial_result = random.choice(results)
+    selected_food = random.choice(results[0])
+    # selected_food = initial_result.str.replace('(','').replace(')','').replace(',','')
+    print(selected_food)
+    return render_template('food.html', selected_food = selected_food, mood_request = mood_request)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_action():
+    email = request.form.get('email')
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+    cur.execute('SELECT id, email, name FROM users WHERE email = %s;', [email])
+    results = cur.fetchone()
+    
+    print(email, 'email')
+    print(email == None, 'results')
+    conn.commit()
+    conn.close()
+
+    if results != None:
+        id = results[0]
+        email = results[1]
+        name = results[2]
+        
+        if results[1] == email:
+            session['id'] = id
+            session['name'] = name
+            print('yes')
+            if id is None:
+                print('none')
+            else:
+                print('hi')
+            return redirect('/')
+    else:
+        print('no')
+        return redirect('/login')
+
+@app.route('/logout')
+def log_out():
+  session.clear()
+  return redirect('/')
    
 
 
